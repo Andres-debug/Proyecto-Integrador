7421c957ff8a29f59916ec5aa3e3ffddbf996594
Andres Diaz <andresf.diazm@unac.edu.co>
2020-06-02 20:31:25 -0500

    Enlace Base de datos
    Se sube la conexion a la base de datos falta las peticiones

---
+++ src/ConexionBaseDatos/ConexionBaseDatos.java
@@ -0,0 +1,29 @@
+package ConexionBaseDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBaseDatos {
+    
+    private static Connection cnx = null;
+   public static Connection obtener() throws SQLException, ClassNotFoundException {
+      if (cnx == null) {
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection("jdbc:mysql://localhost:3307/physicslab?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "Andres", "Andres756144");
+         } catch (SQLException ex) {
+            throw new SQLException(ex);
+         } catch (ClassNotFoundException ex) {
+            throw new ClassCastException(ex.getMessage());
+         }
+      }
+      return cnx;
+   }
+   public static void cerrar() throws SQLException {
+      if (cnx != null) {
+         cnx.close();
+      }
+   }
+
+}
