9e9c6e2fb5f3fc12fb6d74461c1825a841227196
Andres Diaz <andresf.diazm@unac.edu.co>
2020-06-02 20:36:21 -0500

    Creacion de la clase modelo y metodos de peticion
    Se coloco los metodos de peticion a la base de datos

--- src/physicslab/PhysicsLab.java
+++ src/physicslab/PhysicsLab.java
@@ -5,6 +5,15 @@
  */
 package physicslab;
 
+import ConexionBaseDatos.ConexionBaseDatos;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Usuario;
+
 /**
  *
  * @author Jowen
@@ -15,7 +24,60 @@ public class PhysicsLab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            recuperarUsuario(ConexionBaseDatos.obtener(), "adiaz");
+        } catch (SQLException ex) {
+            Logger.getLogger(PhysicsLab.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(PhysicsLab.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void recuperarUsuario(Connection conexion, String user) throws SQLException {
+
+        try {
+            Usuario usuario = new Usuario();
+            PreparedStatement consulta = conexion.prepareStatement("SELECT user, user_name, user_lastname, password FROM user WHERE user = ?");
+            consulta.setString(1, user);
+            ResultSet resultado = consulta.executeQuery();
+            while (resultado.next()) {
+                System.out.println(resultado.getString("user"));
+                System.out.println(resultado.getString("user_name"));
+                System.out.println(resultado.getString("user_lastname"));
+                System.out.println(resultado.getString("password"));
+
+                // usuario.setNombre(resultado.getString("user_name"));
+                // tarea = new Tarea(id_tarea, resultado.getString("titulo"), resultado.getString("descripcion"), resultado.getInt("nivel_de_prioridad"));
+            }
+        } catch (SQLException ex) {
+            throw new SQLException(ex);
+        }
+        // return null;
+    }
+
+    public String registrarUsuario(Connection conexion, Usuario user) throws SQLException {
+        String mensaje = null;
+        try {
+
+            PreparedStatement consulta = conexion.prepareStatement("INSERT INTO user (user_name,user_lastname,user,password) values(?,?,?,?)");
+            consulta.setString(1, user.getNombre());
+            consulta.setString(2, user.getApellido());
+            consulta.setString(3, user.getUsuario());
+            consulta.setString(4, user.getPassword());
+            boolean resultado = consulta.execute();
+
+            if (resultado) {
+                mensaje = "usuario regisgtrado exitosamente";
+            } else {
+                mensaje = "no se pudo registar el usuario";
+            }
+        } catch (SQLException ex) {
+            throw new SQLException(ex);
+        }
+        // return null;
+        return mensaje;
+    }
+
     }
     
 }
